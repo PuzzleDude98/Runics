7f7c03a8536112b8192d16bf18f58c4e33964af1
PuzzleDude98 <puzzledude98@gmail.com>
2024-11-25 09:23:59 -0800

    Initial commit

---
+++ src/main/java/net/pd98/RunicsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pd98;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RunicsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
